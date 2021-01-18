5ce848ec918a0958ebb9c01c18fc02ca10a07e43
Juan villarroel <vilfrack21@gmail.com>
2021-01-18 19:18:21 -0300

    page 25

--- app/src/main/java/com/thebignerdranch/MainActivity.kt
+++ app/src/main/java/com/thebignerdranch/MainActivity.kt
@@ -2,10 +2,18 @@ package com.thebignerdranch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
     }
 }
