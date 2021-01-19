5eb2f06324ffc6c425d0d35ed5c63fbafb9b449b
Juan villarroel <vilfrack21@gmail.com>
2021-01-18 22:26:40 -0300

    Se cambia ubicacion del mensaje toast

--- app/src/main/java/com/thebignerdranch/MainActivity.kt
+++ app/src/main/java/com/thebignerdranch/MainActivity.kt
@@ -2,7 +2,10 @@ package com.thebignerdranch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view:View ->
+            var toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.show()
+        }
+
+        falseButton.setOnClickListener { view:View ->
+            var toast = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.show()
+        }
     }
 }
