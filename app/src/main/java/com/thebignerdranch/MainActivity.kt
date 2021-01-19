2f7692f00ef76da2e03cb1075612484564734d11
Juan villarroel <vilfrack21@gmail.com>
2021-01-18 23:08:38 -0300

    Page 47

--- app/src/main/java/com/thebignerdranch/MainActivity.kt
+++ app/src/main/java/com/thebignerdranch/MainActivity.kt
@@ -5,12 +5,25 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view:View ->
             var toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
@@ -30,5 +45,16 @@ class MainActivity : AppCompatActivity() {
             toast.setGravity(Gravity.TOP,0,0)
             toast.show()
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQUestion()
+        }
+        updateQUestion()
+
+    }
+    private fun updateQUestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
