6a5d615e71b5721ea2b2bcf890eede77e75cf984
Juan villarroel <vilfrack21@gmail.com>
2021-01-21 19:03:20 -0300

    add viewmodel

--- app/src/main/java/com/thebignerdranch/MainActivity.kt
+++ app/src/main/java/com/thebignerdranch/MainActivity.kt
@@ -2,11 +2,18 @@ package com.thebignerdranch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX ="index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +22,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
-
-    private var currentIndex = 0
+    private val quizViewModel:QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -43,19 +47,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQUestion()
+            quizViewModel.moveToNext()
+            updateQuestion()
         }
-        updateQUestion()
+        updateQuestion()
 
     }
-    private fun updateQUestion(){
-        val questionTextResId = questionBank[currentIndex].textResId
+    private fun updateQuestion(){
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
+    override fun onSaveInstanceState(savedInstanceState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(savedInstanceState, outPersistentState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
